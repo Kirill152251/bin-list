1d4e7bbd9ec4d3ae233a3e77a0a0e659da5a3a3c
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-12-21 14:06:17 +0300

    Create GetInfoScreen fragment with viewBinding

---
+++ app/src/main/java/com/bininfo/presentation/GetInfoScreen.kt
@@ -0,0 +1,32 @@
+package com.bininfo.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bininfo.R
+import com.bininfo.databinding.FragmentGetInfoScreenBinding
+
+class GetInfoScreen : Fragment(R.layout.fragment_get_info_screen) {
+
+    private var _binding: FragmentGetInfoScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGetInfoScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
