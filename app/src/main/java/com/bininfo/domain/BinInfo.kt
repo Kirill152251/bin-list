dd4e21ed61f08cb8ba497eccc034c8d6ae97f525
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-12-21 17:24:22 +0300

    Add BinInfo domain model

---
+++ app/src/main/java/com/bininfo/domain/BinInfo.kt
@@ -0,0 +1,11 @@
+package com.bininfo.domain
+
+data class BinInfo(
+    val brand: String,
+    val bank: String,
+    val bankSite: String,
+    val bankPhone: String,
+    val country: String,
+    val lat: Int,
+    val lon: Int
+)
