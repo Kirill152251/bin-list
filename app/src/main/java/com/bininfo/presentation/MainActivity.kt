be6c870681c8e78cd1c1524824257b2dc0800f22
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-12-22 20:48:06 +0300

    Bug fix (edit text hint don't disappear)
    use replace to avoid fragment overlaying

--- app/src/main/java/com/bininfo/presentation/MainActivity.kt
+++ app/src/main/java/com/bininfo/presentation/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container_view, GetInfoScreen())
+                .replace(R.id.fragment_container_view, GetInfoScreen())
                 .commit()
         }
     }
