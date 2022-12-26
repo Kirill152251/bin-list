5c1f35d33dc9c4240df5828ed4bf98bccc2e9a84
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2022-12-26 17:58:58 +0300

    Implement HistoryScreenViewModel and use it in fragment

--- app/src/main/java/com/bininfo/presentation/HistoryScreen.kt
+++ app/src/main/java/com/bininfo/presentation/HistoryScreen.kt
@@ -1,17 +1,36 @@
 package com.bininfo.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bininfo.R
 import com.bininfo.databinding.FragmentHistoryScreenBinding
+import com.bininfo.presentation.view_models.HistoryScreenEvent
+import com.bininfo.presentation.view_models.HistoryScreenState
+import com.bininfo.presentation.view_models.HistoryScreenViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HistoryScreen : Fragment(R.layout.fragment_history_screen) {
 
     private var _binding: FragmentHistoryScreenBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<HistoryScreenViewModel>()
+
+    private val adapter = BinInfoAdapter {
+        viewModel.setEvent(HistoryScreenEvent.DeleteItem(it))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +42,28 @@ class HistoryScreen : Fragment(R.layout.fragment_history_screen) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        viewModel.setEvent(HistoryScreenEvent.FetchHistory)
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state.collect { state ->
+                    when (state) {
+                        is HistoryScreenState.BinInfoHistoryState -> {
+                            binding.textNoHistory.isVisible = false
+                            state.binInfo.collect {
+                                adapter.submitList(it)
+                            }
+                        }
+                        HistoryScreenState.EmptyHistory -> {
+                            binding.textNoHistory.isVisible = true
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/com/bininfo/presentation/view_models/HistoryScreenViewModel.kt
@@ -0,0 +1,68 @@
+package com.bininfo.presentation.view_models
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bininfo.domain.BinInfoHistory
+import com.bininfo.domain.BinInfoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class HistoryScreenViewModel @Inject constructor(
+    private val repository: BinInfoRepository
+) : ViewModel() {
+
+    private val _state = MutableStateFlow<HistoryScreenState>(HistoryScreenState.EmptyHistory)
+    val state = _state.asStateFlow()
+
+    private val _event = MutableSharedFlow<HistoryScreenEvent>()
+    private val event = _event.asSharedFlow()
+
+    init {
+        handleEvent()
+    }
+
+    private fun handleEvent() {
+        viewModelScope.launch {
+            event.collect { event ->
+                when (event) {
+                    HistoryScreenEvent.FetchHistory -> {
+                        val historyFlow = repository.getBinHistory()
+                        _state.value = HistoryScreenState.BinInfoHistoryState(historyFlow)
+                    }
+                    HistoryScreenEvent.DeleteAllHistory -> {
+                        withContext(Dispatchers.IO) {
+                            repository.clearHistory()
+                        }
+                        _state.value = HistoryScreenState.EmptyHistory
+                    }
+                    is HistoryScreenEvent.DeleteItem -> {
+                        withContext(Dispatchers.IO) {
+                            repository.deleteBinInfoFromDb(event.binInfoHistory)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun setEvent(event: HistoryScreenEvent) {
+        val newEvent = event
+        viewModelScope.launch { _event.emit(newEvent) }
+    }
+}
+
+sealed class HistoryScreenState {
+    object EmptyHistory : HistoryScreenState()
+    data class BinInfoHistoryState(val binInfo: Flow<List<BinInfoHistory>>) : HistoryScreenState()
+}
+
+sealed class HistoryScreenEvent {
+    object FetchHistory : HistoryScreenEvent()
+    data class DeleteItem(val binInfoHistory: BinInfoHistory) : HistoryScreenEvent()
+    object DeleteAllHistory : HistoryScreenEvent()
+}
